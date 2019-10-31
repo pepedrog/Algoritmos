2a3614b1c87cd253a54b9d130303a9b97b5a237f
Pedro Gigeck <gigeck.pedro@usp.br>
2019-10-31 14:18:20 -0300

    ajustando layout e comentarios

--- PD/nitidez.c
+++ PD/nitidez.c
@@ -16,6 +16,7 @@
 //                                                                                                    //
 // que deve ser não-negativo para que as palavras caibam na linha. Desejamos minimizar a soma,        //
 // sobre todas as linhas exceto a última, do cubo do número de espaços extras no final das linhas.    //
+// Algoritmo consome O(n³) unidades de tempo.
 //                                                                                                    //
 // Problema 15-2 do CLRS                                                                              //
 //                                                                                                    //
@@ -29,16 +30,16 @@
 int DEBUG = 0;
 
 // Função auxiliar a³
-int cubo( int a) {
+int cubo (int a) {
     return a*a*a;
 }
 
 // Imprime as n palavras em l linhas de até M caracteres
 // do jeito nítido indicado pela tabela veio 
-void imprimeTela( char* palavra[], int** veio, int* sp, int n, int M, int linha) {
+void imprimeTela (char* palavra[], int** veio, int* sp, int n, int M, int linha) {
     
     // Pilha pra inverter a ordem das linhas
-    int* pilha = malloc( linha * sizeof( int));
+    int* pilha = malloc (linha * sizeof( int));
     int tam = 0;
     pilha[tam++] = veio[linha--][n - 1];
     while (linha >= 0) {
@@ -49,36 +50,36 @@ void imprimeTela( char* palavra[], int** veio, int* sp, int n, int M, int linha)
 
     // Para cada linha
     for (int l = tam - 1; l > 0; l--) {
-        printf("| ");
+        printf ("| ");
         // Imprime as palavras dessa linha
-        for (int k = pilha[l] + 1; k <= pilha[l - 1]; k++) printf( "%s ", palavra[k]);
+        for (int k = pilha[l] + 1; k <= pilha[l - 1]; k++) printf ("%s ", palavra[k]);
         // Coloca os espaços em branco no final
         int espbranco = M - pilha[l - 1] + pilha[l] - sp[pilha[l - 1]] + sp[pilha[l]];
-        for (int i = 0; i < espbranco; i++) printf( " "); printf( " |\n");
+        for (int i = 0; i < espbranco; i++) printf (" "); printf (" |\n");
     }
     // Imprime a ultima linha
     printf( "| ", palavra[pilha[0]]);
-    for (int k = pilha[0] + 1; k < n; k++) printf( "%s ", palavra[k]);
+    for (int k = pilha[0] + 1; k < n; k++) printf ("%s ", palavra[k]);
     int espbranco = M - n + 1 + pilha[0] - sp[n-1] + sp[pilha[0]];
-    for (int i = 0; i < espbranco; i++) printf( " "); printf( " |\n");
+    for (int i = 0; i < espbranco; i++) printf (" "); printf (" |\n");
 
-    free( pilha);
+    free (pilha);
 }
 
 // Printa a tabela da Programação Dinâmica
-// Que indica o custo de cada impressão
-void printaTabela( int** custo, int** veio, int n) {
+// Que indica o custo de cada impressão (para debugar)
+void printaTabela (int** custo, int** veio, int n) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; j++) 
-            if (custo[i][j] == INF) printf( "INF\t"); 
-            else printf( "%d|%d\t", custo[i][j], veio[i][j]);
-        printf( "\n");
+            if (custo[i][j] == INF) printf ("INF\t"); 
+            else printf ("%d|%d\t", custo[i][j], veio[i][j]);
+        printf ("\n");
     }
 }
 
 // Calcula com um algoritmo de programação dinâmica
 // Um jeito nítido de imprimir as n palavras
-int imprimeNitido( char* palavra[], int* l, int* sp, int n, int M) {
+int imprimeNitido (char* palavra[], int* l, int* sp, int n, int M) {
 
     // Se cabe tudo na primeira linha, o custo é 0
     if (M >= sp[n-1] + n - 1)
@@ -89,17 +90,17 @@ int imprimeNitido( char* palavra[], int* l, int* sp, int n, int M) {
     // A tabela custo terá a forma:
     // custo[li][k] := melhor custo de imprimir k palavras em li linhas (INF se for impossível)
     // veio[li][k] := última palavra da linha li - 1 (para rastrear a PD)
-    int** custo = malloc( n * sizeof( int*));
-    int** veio = malloc( n * sizeof( int*));
+    int** custo = malloc (n * sizeof( int*));
+    int** veio = malloc (n * sizeof( int*));
     for (int i = 0; i < n; i++){
-        custo[i] = malloc( n * sizeof( int));
-        veio[i] = malloc( n * sizeof( int));
+        custo[i] = malloc (n * sizeof( int));
+        veio[i] = malloc (n * sizeof( int));
     }
 
     // Inicialização da primeira linha
     for (int k = 0; k < n; k++) {
         // Calculo dos espaços em branco
-        custo[0][k] = cubo( M - k - sp[k]);
+        custo[0][k] = cubo (M - k - sp[k]);
         // Se a palavra não cabe na linha
         if (custo[0][k] < 0) custo[0][k] = INF;
     }
@@ -113,7 +114,7 @@ int imprimeNitido( char* palavra[], int* l, int* sp, int n, int M) {
             // Calcula o custo conforme a formula da recorrência
             // custo[li][k] = min_i{ M - k + i + 1 - (l[i] + ... + l[k]) }
             for (int i = li - 1; i < k; i++) {
-                int clinha = cubo( M - k + i + 1 - (sp[k] - sp[i]));
+                int clinha = cubo (M - k + i + 1 - (sp[k] - sp[i]));
                 // Não considera o custo da última linha (linha da ultima palavra)
                 if (k == n - 1 && clinha > 0) clinha = 0;
                 // Se a palavra couber na linha e a linha anterior for valida (pra não dar overflow)
@@ -137,23 +138,23 @@ int imprimeNitido( char* palavra[], int* l, int* sp, int n, int M) {
         }
 
     // Ótimo para debugar
-    if (DEBUG) printaTabela( custo, veio, n);
+    if (DEBUG) printaTabela (custo, veio, n);
+    // Imprime o resultado
+    imprimeTela (palavra, veio, sp, n, M, linha);
 
-    imprimeTela( palavra, veio, sp, n, M, linha);
-    
+    // Libera a memória
     for (int i = 0; i < n; i++) {
-        free( custo[i]);
-        free( veio[i]);
+        free (custo[i]);
+        free (veio[i]);
     }
-    free( custo);
-    free( veio);
+    free (custo);
+    free (veio);
 
     return menor;
 
 }
 
-// Versão para debugar com as palavras
-int main( int argc, char* argv[]) {
+int main (int argc, char* argv[]) {
 
     int n, M;
     int* l;
@@ -162,15 +163,16 @@ int main( int argc, char* argv[]) {
 
     if (argc > 1) DEBUG = 1;
 
-    scanf( "%d", &n);
-    palavra = malloc( n * sizeof( char* ));
-    l = malloc( n* sizeof( int));
-    sp = malloc( n * sizeof( int));
+    scanf ("%d", &n);
+    palavra = malloc (n * sizeof( char* ));
+    l = malloc (n* sizeof( int));
+    sp = malloc (n * sizeof( int));
 
+    // Le as palavras
     for (int i = 0; i < n; i++) {
         l[i] = 0;
-        palavra[i] = malloc( 15 * sizeof( char));
-        // Le a palavra e guarda o tamanho
+        palavra[i] = malloc (30 * sizeof( char));
+        // Le a palavra e guarda o tamanho delas
         char c = getchar() ;
         while (c == ' ' || c == '\n') c = getchar();
         while (c != ' ' && c != '\n' && c != '\0') {
@@ -182,11 +184,9 @@ int main( int argc, char* argv[]) {
         else sp[i] = l[i];
     }
 
-    scanf("%d", &M);
+    scanf ("%d", &M);
 
-    int custo = imprimeNitido( palavra, l, sp, n, M);
-    if (DEBUG) printf( "Custo da impressão: %d\n", custo);
+    int custo = imprimeNitido (palavra, l, sp, n, M);
+    printf ("Custo da impressão: %d\n", custo);
     return 0;
 }
-
-// Algoritmo Cúbico em n
