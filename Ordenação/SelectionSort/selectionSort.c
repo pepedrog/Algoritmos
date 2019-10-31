dbb070a2b2c8fdb45dc0df6827f5e49ea6ac670e
Pedro Gigeck <gigeck.pedro@usp.br>
2019-10-31 14:38:59 -0300

    comentario inicial do selectionSort

--- Ordenação/SelectionSort/selectionSort.c
+++ Ordenação/SelectionSort/selectionSort.c
@@ -1,26 +1,32 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                     //
+//                             Ordenação por Seleção                                   //
+//                                                                                     //
+// Algoritmo de ordenação baseado na seguinte ideia:                                   //
+// - Manter o começo do vetor ordenado (começa vazio)                                  //
+// - Procurar o menor elemento da parte não ordenada                                   //
+// - Colocar esse menor elemento no final da parte ordenada                            //
+// - Repetir isso até que todos os elementos estejam na parte ordenada                 //
+//                                                                                     //
+// É um algoritmo de ordenação estável e consome tempo O(n^2) em qualquer caso.        //
+//                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////
+
 #include "../FunçõesAuxiliares/compara.h" // Funções de comparação pros tipos primitivos
-#include "../vetor.h" // Definião do vetor e troca()
+#include "../vetor.h" // Definição do vetor e troca()
 
 #define v(i) ((char *) v + i * sz)
 
 // Ordena o vetor v com a ordem estabelecida pela função compara
 void selectionSort (vetor v, int n, size_t sz, int (*compara) (const void *, const void *)) {
-
-    int i; // Primeiro índice da parte não ordenada
-    int j; // Iterador sob a parte não ordenada
-    int min; // Guarda o menor elemento da parte não ordenada
-    // Vai aumentando a parte ordenada, até ser o vetor inteiro
+    int i, j, min;
+    // Vai aumentando a parte ordenada
     for (i = 0; i < n; i++) {
-
         min = i;
-        // Percorre a parte não ordenada procurando o menor elemento
+        // Procurando o menor elemento do resto
         for (j = i + 1; j < n; j++)
-            // Se j < min, min = j
-
-            printf ("comparando %d com %d\n", *((int *) v(j)), *((int *) v(min)));
-            if (compara (v(j), v(min)) < 0) printf(" j < min\n"), min = j;
-        // Coloca mínimo na parte ordenada
-        printf ("colocando %d na parte ordenada\n", *((int *) v(min)));
+            if (compara (v(j), v(min)) < 0) min = j;
+        // Coloca o menor na parte ordenada
         troca (v, i, min, sz);  
     }
 }
