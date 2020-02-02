de2b036d09251ae4e951834808cbf56d8fce8682
pepedrog <gigeck.pedro@usp.br>
2020-02-01 21:25:47 -0300

    100 linhas em 20 com macro muito legal no teste

--- Ordenação/Testes/teste.c
+++ Ordenação/Testes/teste.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include "../vetor.h"
 
+// Macro muito util pra não recopiar esse trecho pra cada função
+#define processa(nome)             \
+    copiaVetor (ordena, v, n, sizeof (int)); \
+    t = clock();                    \
+    ordenaTipo(ordena, n, int, nome); \
+    printf ("%s:  \t%fs\n", #nome, (clock() - t) / (float) CLOCKS_PER_SEC); \
+    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n")
+
 int main (int argc, char *argv[]) {
     int n, k;
 
@@ -16,47 +24,13 @@ int main (int argc, char *argv[]) {
 
     vetor ordena = clonaVetor (v, n, sizeof (int));
     
-    t = clock();
-    ordenaTipo(ordena, n, int, quickSort);
-    printf ("QuickSort 0:   %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
-
-    copiaVetor (ordena, v, n, sizeof (int));
-    t = clock();
-    ordenaTipo(ordena, n, int, quickSortOtim1);
-    printf ("QuickSort 1:   %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
-
-    copiaVetor (ordena, v, n, sizeof (int));
-    t = clock();
-    ordenaTipo(ordena, n, int, quickSortOtim2);
-    printf ("QuickSort 2:   %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
-
-    copiaVetor (ordena, v, n, sizeof (int));
-    t = clock();
-    ordenaTipo(ordena, n, int, quickSortOtim3);
-    printf ("QuickSort 3:   %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
-
-    copiaVetor (ordena, v, n, sizeof (int));
-    t = clock();
-    ordenaTipo(ordena, n, int, qsort);
-    printf ("QuickSort C:   %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
-
-    
-    copiaVetor (ordena, v, n, sizeof (int));
-    t = clock();
-    ordenaTipo(ordena, n, int, mergeSort);
-    printf ("MergeSort:     %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
-
-    copiaVetor (ordena, v, n, sizeof (int));
-    t = clock();
-    ordenaTipo(ordena, n, int, heapSort);
-    printf ("HeapSort:      %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n"), INTprintaVetor(ordena, n);
+    processa(quickSort);
+    processa(quickSortOtim1);
+    processa(quickSortOtim2);
+    processa(quickSortOtim3);
+    processa(qsort);
+    processa(mergeSort);
+    processa(heapSort);
 
     copiaVetor (ordena, v, n, sizeof (int));
     t = clock();
@@ -64,28 +38,11 @@ int main (int argc, char *argv[]) {
     printf ("CountingSort:  %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
     if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
 
-    t = clock();
-    ordenaTipo(ordena, n, int, selectionSort);
-    printf ("SelectionSort: %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
-
-    copiaVetor (ordena, v, n, sizeof (int));
-    t = clock();
-    ordenaTipo(ordena, n, int, insertionSort);
-    printf ("InsertionSort: %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
-
-    copiaVetor (ordena, v, n, sizeof (int));
-    t = clock();
-    ordenaTipo(ordena, n, int, cocktailSort);
-    printf ("CocktailSort:  %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
-
-    copiaVetor (ordena, v, n, sizeof (int));
-    t = clock();
-    ordenaTipo(ordena, n, int, bubbleSort);
-    printf ("BubbleSort:    %fs\n", (clock() - t) / (float) CLOCKS_PER_SEC);
-    if (!ordenaTipo(ordena, n, int, estaOrdenado)) printf ("ERRO: vetor não ordenado\n");
+    
+    processa(selectionSort);
+    processa(insertionSort);
+    processa(cocktailSort);
+    processa(bubbleSort);
 
     free (ordena);
     free (v);
